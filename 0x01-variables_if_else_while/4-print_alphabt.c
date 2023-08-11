68c141f17a95dbcb005747f21e7e93198af4eb84
Bahy Allam <bahy.alam@yahoo.com>
2023-08-11 16:57:43 -0700

    Add 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c == 'e' || c == 'q')
+			continue;
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
