ea8c700c44b09e6207d2b2772e6e8feb79106ed0
Bahy Allam <bahy.alam@yahoo.com>
2023-08-11 18:00:40 -0700

    Add 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int d = 0, c;
+
+	while (d <= 9)
+	{
+		c = 0;
+		while (c <= 9)
+		{
+			if (d != c && d < c)
+			{
+				putchar(d + 48);
+				putchar(c + 48);
+
+			}
+			c++;
+		}
+
+		if (d != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		d++;
+	}
+	putchar('\n');
+
+	return (0);
+}
