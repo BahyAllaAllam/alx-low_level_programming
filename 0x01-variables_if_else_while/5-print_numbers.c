91969063c1828b95b3d503f6d7ea3886c2e8c975
Bahy Allam <bahy.alam@yahoo.com>
2023-08-11 17:11:38 -0700

    Add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int d = 0;
+
+	while (d <= 9)
+	{
+		printf("%d", c);
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
