9ed6214fdf787222b51bf21a40ba27425b444c75
Bahy Allam <bahy.alam@yahoo.com>
2023-08-11 17:34:24 -0700

    Add 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	int d = 0;
+
+	while (d <= 9)
+	{
+		putchar(d + 48);
+
+		if (d != 9)
+			putchar(',');
+			putchar(' ');
+
+		d++;
+	}
+	putchar('\n');
+
+	return (0);
+}
