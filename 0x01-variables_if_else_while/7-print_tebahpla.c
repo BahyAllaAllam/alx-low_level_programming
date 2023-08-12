607839c28ca62897ab10b98f83bbf05d19600a3a
Bahy Allam <bahy.alam@yahoo.com>
2023-08-11 17:19:49 -0700

    Add 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
