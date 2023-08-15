c9ac08e52be80c4f2c2365ea11f231b1a78a130c
Bahy Allam <bahy.alam@yahoo.com>
2023-08-15 16:28:43 -0700

    Add 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * main - entry point.
+ *
+ * Description: false or true validation.
+ *
+ * Return: 0 (Success).
+*/
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 int _putchar(char c);
+void print_alphabet(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
