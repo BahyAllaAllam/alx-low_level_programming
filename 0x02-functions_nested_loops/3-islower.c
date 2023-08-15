c71612789bb00494d91e7d2c26882f075890cf5c
Bahy Allam <bahy.alam@yahoo.com>
2023-08-15 16:40:46 -0700

    Add 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - entry point.
+ *
+ * @c: false or true validation.
+ *
+ * Return: 0 (Success).
+*/
+
+int _islower(int c)
+{
+	if (x >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
