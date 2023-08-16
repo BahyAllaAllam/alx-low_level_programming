445627058d97c558f3fef139fed7b72f63986968
Bahy Allam <bahy.alam@yahoo.com>
2023-08-15 18:29:04 -0700

    Add 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table - entry point.
+ *
+ * Description: false or true validation.
+ *
+ * Return: 0 (Success).
+*/
+
+void times_table(void)
+{
+	int num1, num2, res;
+
+	for (num1 = 0; num1 <= 9; num1++)
+	{
+		for (num2 = 0; num2 <= 9; num2++)
+		{
+			res = num1 * num2;
+			_putchar(res);
+			if (res == 81)
+			{
+				break;
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+	}	_putchar('\n');
+}
