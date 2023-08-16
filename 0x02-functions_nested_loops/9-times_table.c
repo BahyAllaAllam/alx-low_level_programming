8ce25bc34708bfb82c466fadffb24bf4943be3bc
Bahy Allam <bahy.alam@yahoo.com>
2023-08-15 18:36:13 -0700

    Hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,13 +10,15 @@
 
 void times_table(void)
 {
-	int num1, num2, res;
+	int num1, num2;
 
 	for (num1 = 0; num1 <= 9; num1++)
 	{
 		for (num2 = 0; num2 <= 9; num2++)
 		{
+			int res
 			res = num1 * num2;
+			char res
 			_putchar(res + 48);
 			if (res == 81)
 			{
