42d0be667092d66f0e71a3897426f87893149d36
Bahy Allam <bahy.alam@yahoo.com>
2023-08-15 19:54:33 -0700

    Add 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - entry point.
+ *
+ * @a: false or true validation.
+ * @b: second.
+ * Return: 0 (Success).
+*/
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
