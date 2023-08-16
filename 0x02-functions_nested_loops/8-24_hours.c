4c8a5c811a7e576538af04f21ac98a768acad8a8
Bahy Allam <bahy.alam@yahoo.com>
2023-08-15 17:36:25 -0700

    Add 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - entry point.
+ *
+ * Description: false or true validation.
+ *
+ * Return: 0 (Success).
+*/
+
+void jack_bauer(void)
+{
+	int m, h;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			_putchar((h / 10) + 48);
+			_putchar((h % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
