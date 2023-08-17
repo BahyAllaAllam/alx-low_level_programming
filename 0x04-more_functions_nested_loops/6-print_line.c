db1e59ecae216fcdae970d45b069bd735213db68
Bahy Allam <bahy.alam@yahoo.com>
2023-08-17 16:43:15 -0700

    Add 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - check the code
+ *
+ * @n: n
+ *
+ * Return: Always 0.
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (int i; i <= n; i++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
