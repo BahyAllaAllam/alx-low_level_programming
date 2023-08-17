4670978f8881ea9ac457eca450d1f3677217149a
Bahy Allam <bahy.alam@yahoo.com>
2023-08-17 15:41:39 -0700

    Add 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	for (i = 48; i >= 57; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n')
+}
