f322b5a9b486e65f4935cb8411362041fa1cf8fe
Bahy Allam <bahy.alam@yahoo.com>
2023-08-17 16:30:40 -0700

    Hot fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,12 @@ void more_numbers(void)
 {
 	int i, x, num;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 1; i <= 10; i++)
 	{
 		for (x = 0; x <= 14; x++)
 		{
 			num = x;
-			if (x >= 10)
+			if (x > 9)
 			{
 				_putchar(1 + 48);
 				num = x % 10;
