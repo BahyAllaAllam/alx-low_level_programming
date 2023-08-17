e6e425930a245b12da3d06ac47b5ebcd6f7e1b95
Bahy Allam <bahy.alam@yahoo.com>
2023-08-17 15:18:13 -0700

    second commit

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * _putchar - entry point.
+ *
+ * @c: false or true validation.
+ *
+ * Return: 0 (Success).
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
