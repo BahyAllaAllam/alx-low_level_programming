5ef4f225aa54b7952523f209a7a41baab7cc8c2d
Bahy Allam <bahy.alam@yahoo.com>
2023-08-17 17:03:21 -0700

    Add 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - check the code
+ *
+ * @n: n
+ *
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+	int i, x;
+
+	if (n <= 0)
+		_putcahr('\n');
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (x = 1; x <= i; x++)
+			{
+				_putcahr(' ');
+			}
+			_putcahr('\\');
+			_putcahr('\n');
+		}
+	}
+}
