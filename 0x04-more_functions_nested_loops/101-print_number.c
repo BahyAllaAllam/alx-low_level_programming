feae3ad516158c81b6d35c221ff0c867b4563fa4
Bahy Allam <bahy.alam@yahoo.com>
2023-08-17 18:28:36 -0700

    Add 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - check the code
+ *
+ * @n: n
+ *
+ * Return: Always 0.
+*/
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -i;
+	}
+
+	if ((i / 10) > 0)
+		print_number(i / 10);
+	_putchar((i % 10) + 48);
+}
