f7869fe45d9507aacfbf279bcc7196109e62bcb5
Bahy Allam <bahy.alam@yahoo.com>
2023-08-21 11:25:44 -0700

    Add 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - check the code
+ *
+ * @a: a
+ *
+ * @b: b
+ *
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int s = *a;
+	*a = *b;
+	*b = s;
+}
