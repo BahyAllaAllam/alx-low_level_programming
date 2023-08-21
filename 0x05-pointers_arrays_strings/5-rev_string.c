1b2c654883564f240d3733a1809e3cecf3453f64
Bahy Allam <bahy.alam@yahoo.com>
2023-08-21 15:34:47 -0700

    Hot fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,7 +13,7 @@ void rev_string(char *s)
 {
 	int i = 0;
 	int len = strlen(s) - 1;
-	char new_s;
+	char new_s[];
 
 	while (i <= s[len + 1])
 	{
@@ -21,5 +21,5 @@ void rev_string(char *s)
 		len--;
 		i++;
 	}
-	return (new_s);
+	printf("%s", new_s);
 }
