a34575818d35c19c58c3cb2c40add4ff58d55cc6
Bahy Allam <bahy.alam@yahoo.com>
2023-08-21 18:59:46 -0700

    Add 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _atoi - check the code
+ *
+ * @s: s
+ *
+ * Return: Always 0.
+ */
+
+int _atoi(char *s)
+{
+	printf("%d\n", atoi(s));
+}
