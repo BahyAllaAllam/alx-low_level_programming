a04d620ef85f46818d1e5b36fa3f784d2acc3eec
Bahy Allam <bahy.alam@yahoo.com>
2023-08-21 17:15:21 -0700

    Add 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - check the code
+ *
+ * @str: str
+ *
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int i = 0, len = strlen(s) - 1;
+
+	while (s[i])
+	{
+		putchar(s[i]);
+		i = i + 2;
+	}
+}
