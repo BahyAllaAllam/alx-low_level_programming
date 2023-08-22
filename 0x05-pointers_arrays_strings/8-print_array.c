dc556dbecac55b4e950389a99a71af2225af6026
Bahy Allam <bahy.alam@yahoo.com>
2023-08-21 18:30:18 -0700

    Hot fix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -16,7 +16,12 @@ void print_array(int *a, int n)
 
 	while (i < n)
 	{
-		printf("%d, ", a[i]);
+		if (i == n - 1)
+		{
+			printf("%d", a[i]);
+		}
+		else
+			printf("%d, ", a[i]);
 		i++;
 	}
 	putchar('\n');
