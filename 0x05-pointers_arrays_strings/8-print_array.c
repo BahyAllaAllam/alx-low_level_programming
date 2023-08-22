4d1ff1fbdb4c97a405b7cd8bb8cf9eb97d21465a
Bahy Allam <bahy.alam@yahoo.com>
2023-08-21 18:25:34 -0700

    Add 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_array - check the code for
+ *
+ * @a: a
+ *
+ * @n: n
+ *
+ * Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		i++;
+	}
+	putchar('\n');
+}
