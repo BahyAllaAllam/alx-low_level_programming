98d15c2dd19d8d84f038c55ac337a260b519e032
Bahy Allam <bahy.alam@yahoo.com>
2023-08-28 12:49:03 -0700

    Add 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * set_string - prints buffer in hexa
+ *
+ * @s: the address of memory to print
+ *
+ * @to: size
+ *
+ * Return: Nothing.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
