e6c00b8ce96c33c20f561938a54443bb2c9b71d2
Bahy Allam <bahy.alam@yahoo.com>
2023-08-30 16:21:42 -0700

    Add 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * is_palindrome - check the code
+ *
+ * @s: s
+ *
+ * Return: Always 0.
+ */
+
+int is_palindrome(char *s)
+{
+	int len = strlen(s) - 1;
+
+	if (s == "")
+		return (1);
+	else if (s[0] == s[len] && s[1] == s[len - 1])
+		return (1);
+
+	return (0);
+}
