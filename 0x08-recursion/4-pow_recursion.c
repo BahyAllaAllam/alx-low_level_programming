0279630767bb78ef0e5b94b9542679f8803a1828
Bahy Allam <bahy.alam@yahoo.com>
2023-08-30 13:23:27 -0700

    Hot fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,12 @@
 #include "main.h"
 #include <math.h>
+#include <string.h>
 
 /**
  * _pow_recursion - check the code
  *
  * @x: x
+ *
  * @y: y
  *
  * Return: Always 0.
@@ -13,8 +15,7 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-	{
 		return (-1);
-	}
+
 	return (pow(x, y));
 }
