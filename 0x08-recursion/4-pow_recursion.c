d6595a4278e3d38242f641af0fb5f4cdc6db268b
Bahy Allam <bahy.alam@yahoo.com>
2023-08-30 13:16:47 -0700

    Add 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _pow_recursion - check the code
+ *
+ * @x: x
+ * @y: y
+ *
+ * Return: Always 0.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (pow(x, y));
+}
