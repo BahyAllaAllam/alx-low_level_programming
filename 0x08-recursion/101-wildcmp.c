797377bfdadca1b0a5581d94c4155c3fe2e9c5e0
Bahy Allam <bahy.alam@yahoo.com>
2023-08-30 17:38:50 -0700

    Add 101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,73 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * move_past_star - check the code
+ *
+ * @s2: s2
+ *
+ * Return: Always 0.
+ */
+
+char *move_past_star(char *s2)
+{
+	if (*s2 == '*')
+		return (move_past_star(s2 + 1));
+	return (s2);
+}
+
+/**
+ * wildcmp - check the code
+ *
+ * @s1: s1
+ * @s2: s2
+ *
+ * Return: Always 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
+		return (1);
+	if (*s1 == *s2)
+	{
+		if (!*s1)
+			return (1);
+		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+	}
+	if (!*s1 || !s2)
+		return (0);
+	if (*s2 == '*')
+	{
+		s2 = move_past_star(s2);
+
+		if (!*s2)
+			return (1);
+		if (*s1 == *s2)
+			ret += wildcmp(s1 + 1, s2 + 1);
+		ret += inception(s1, s2);
+		return (!!ret);
+	}
+	return (0);
+}
+
+/**
+ * inception - check the code
+ *
+ * @s1: s1
+ * @s2: s2
+ *
+ * Return: Always 0.
+ */
+int inception(char *s1, char *s2)
+{
+	int ret = 0;
+
+	if (*s1 == 0)
+		return (0);
+	if (*s1 == *s2)
+		ret += wildcmp(s1 + 1, s2 + 1);
+	ret += inception(s1 + 1, s2);
+	return (ret);
+}
