0aa4be6be540b9934e1f460fd60f85f8c168b96e
Bahy Allam <bahy.alam@yahoo.com>
2023-09-01 15:51:41 -0700

    Hot fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,11 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-    if (argc != 3){
-        puts("Error\n");
-        return 1;
-    }
-    printf("%d\n", atoi(argv[1]) * atoi(argv[2]));;
-    return 0;
+/**
+ * main - prints buffer in hexa
+ *
+ * @argc: the address of memory to print
+ *
+ * @argv: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		puts("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
