1a59c7b6c3c1068c3dead97ff1d7f0c1f9a8f163
Bahy Allam <bahy.alam@yahoo.com>
2023-09-01 15:47:51 -0700

    Hot fix

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    int i = 0;
-    
-    while (i < argc){
-        printf("%s\n", argv[i]);;
-        i++;
-    }
-    return 0;
+/**
+ * main - prints buffer in hexa
+ *
+ * @argc: the address of memory to print
+ *
+ * @argv: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+int main(int argc, char const *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
 }
