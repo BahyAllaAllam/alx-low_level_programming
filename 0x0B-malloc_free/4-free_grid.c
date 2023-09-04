0119b79d0d07bed609728ce60a1a8b2e5d7daa85
Bahy Allam <bahy.alam@yahoo.com>
2023-09-04 07:39:16 -0700

    Add 4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_gird - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ *
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+
+void free_gird(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height)
+	{
+		free(gird[i]);
+		i++;
+	}
+	free(grid);
+}
