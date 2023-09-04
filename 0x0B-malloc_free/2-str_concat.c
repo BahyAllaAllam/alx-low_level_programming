997a5474157a7bee31b64c4c7ab3ee9a76922fca
Bahy Allam <bahy.alam@yahoo.com>
2023-09-04 01:48:02 -0700

    Hot fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,9 +19,9 @@ char *str_concat(char *s1, char *s2)
 	char *m;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "";
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
