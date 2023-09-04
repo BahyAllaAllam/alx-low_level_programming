29219f726f30c24e3b36b1ce8db00a71a44fe0c1
Bahy Allam <bahy.alam@yahoo.com>
2023-09-03 22:55:33 -0700

    First commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - prints buffer in hexa
+ * @c: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	char *p = (char *)malloc(sizeof(char) * size);
+	int i = 0;
+
+	while (i < size)
+	{
+		p[i] = 'H';
+		i++;
+	}
+	return (p);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This is the readme file for the task 0x0B-malloc_free
