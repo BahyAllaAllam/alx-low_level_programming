2ebb946b1ca930a11c0a8a9429ea8354f37842d1
Bahy Allam <bahy.alam@yahoo.com>
2023-09-04 07:33:48 -0700

    Add 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - prints a grid of integers
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+*/
+
+int **alloc_grid(int width, int height)
+{
+	int **tab, i, j;
+
+	tab = malloc(sizeof(*tab) * height);
+
+	if (width <= 0 || height <= 0 || tab == 0)
+	{
+		return (NULL);
+	}
+
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			tab[i] = malloc(sizeof(**tab) * width);
+			if (tab[i] == 0)
+			{
+				while (i--)
+				{
+					free(tab[i]);
+				}
+				free(tab);
+				return (NULL);
+			}
+			for (j = 0; j < width; j++)
+			{
+				tab[i][j] = 0;
+			}
+		}
+	}
+	return (tab);
+}
