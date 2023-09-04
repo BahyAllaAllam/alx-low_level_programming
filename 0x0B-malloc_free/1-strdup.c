900a9a56d7968e306b6a4afeb92dbde6d79855f8
Bahy Allam <bahy.alam@yahoo.com>
2023-09-03 23:45:06 -0700

    Hot fix

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,7 +16,7 @@ char *_strdup(char *str)
 	int i, len = strlen(str);
 	char *p = malloc(len);
 
-	if (len == 0 || p == 0)
+	if (str == NULL || p == 0)
 		return (NULL);
 	for (i = 0; i < len; i++)
 		p[i] = str[i];
