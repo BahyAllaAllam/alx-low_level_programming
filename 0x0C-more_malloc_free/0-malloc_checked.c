ac7e4681ac6c3d74b2901a32a0f634671fe541a5
Bahy Allam <bahy.alam@yahoo.com>
2023-09-06 23:54:11 -0700

    First commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - check the code
+ *
+ * @b: b
+ *
+ * Return: Always 0.
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This is the readme file for the task 0x0C-more_malloc_free
