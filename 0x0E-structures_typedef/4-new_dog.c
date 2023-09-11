10cd16b21df3c25c953ec7737f1052bc2e8cad8c
Bahy Allam <bahy.alam@yahoo.com>
2023-09-11 13:37:33 -0700

    Hot fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,10 +16,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *dog;
 
 	if (!name || age < 0 || !owner)
-		return (0);
+		return (NULL);
 	dog = (dog_t *) malloc(sizeof(dog_t));
 	if (!dog)
-		return (0);
+		return (NULL);
 	(*dog).name = malloc(sizeof(char) * strlen(name) + 1);
 	if ((*dog).name == NULL)
 	{
