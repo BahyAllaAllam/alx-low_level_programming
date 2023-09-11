5a8cac7b2f73158c9096229e651ca50ddd90b860
Bahy Allam <bahy.alam@yahoo.com>
2023-09-11 12:47:34 -0700

    Add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - check the code
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: description
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
