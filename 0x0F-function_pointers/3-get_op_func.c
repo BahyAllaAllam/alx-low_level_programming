c34fe26e2a8d70b7c3a03e319b83920ddba7cb45
Bahy Allam <bahy.alam@yahoo.com>
2023-09-13 11:20:41 -0700

    Hot fix

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,9 @@
 #include "3-calc.h"
+#include "function_pointers.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 /**
  * get_op_func - get op func
  * @s: s
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,8 @@
 #include "3-clac.h"
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
