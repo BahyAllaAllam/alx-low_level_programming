b6c7856dbedda86ce0e37f24ffc43768205e18dc
Bahy Allam <bahy.alam@yahoo.com>
2023-09-13 08:38:39 -0700

    Hot fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,3 +1,4 @@
+#include "function_pointers.h"
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,7 +18,8 @@ int main(int argc, char **argv)
 
 	if (argc != 4)
 	{
-		printf("Error\n"), exit(98);
+		printf("Error\n");
+		exit(98);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
@@ -25,11 +27,13 @@ int main(int argc, char **argv)
 	op_func = get_op_func(argv[2]);
 	if (!op_func)
 	{
-		printf("Error\n"), exit(99);
+		printf("Error\n");
+		exit(99);
 	}
 	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
-		printf("Error\n"), exit(100);
+		printf("Error\n");
+		exit(100);
 	}
 	printf("%d\n", op_func(a, b));
 	return (0);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,11 @@
 #include "3-clac.h"
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
 /**
  * op_add - adds two integers
  * @a: a
