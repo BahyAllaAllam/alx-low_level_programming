34fd7b0a3f397eeae7ea8d22d2b9642a121e7820
Bahy Allam <bahy.alam@yahoo.com>
2023-09-13 02:06:29 -0700

    First commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: f
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f && name)
+	{
+		f(name);
+	}
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This is the readme file for the task 0x0F-function_pointers
