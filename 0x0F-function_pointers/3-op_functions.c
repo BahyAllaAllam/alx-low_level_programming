59c18fb81148b6800d5ec27f230158363a80777e
Bahy Allam <bahy.alam@yahoo.com>
2023-09-13 07:41:48 -0700

    Add task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-clac.h"
+
+/**
+ * op_add - adds two integers
+ * @a: a
+ * @b: b
+ *
+ * Return: the sum
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract two integers
+ * @a: a
+ * @b: b
+ *
+ * Return: the substraction
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiple two integers
+ * @a: a
+ * @b: b
+ *
+ * Return: the multiplcation
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide two integers
+ * @a: a
+ * @b: b
+ *
+ * Return: the division
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - the mod of two integers
+ * @a: a
+ * @b: b
+ *
+ * Return: the mod
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
