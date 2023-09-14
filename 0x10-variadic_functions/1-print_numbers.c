9dbff874d2688586f811a245cf4eeb66637d37ef
Bahy Allam <bahy.alam@yahoo.com>
2023-09-14 02:16:33 -0700

    Add 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - check the code
+ * @separator: separator
+ * @n: n
+ *
+ * Return: void
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i = 0;
+	va_list ptr;
+
+	va_start(ptr, n);
+	for (; i < n; i++)
+	{
+		printf("%d", va_arg(ptr, int));
+		if (separator)
+		{
+			printf("%s", separator);
+		}
+	}
+	putchar('\n');
+	va_end(ptr);
+}
