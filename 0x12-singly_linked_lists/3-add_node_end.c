ed83212059936a30c1dd7050be4422847bf85e42
BahyAllaAllam <bahy.alam@yahoo.com>
2023-09-21 16:34:03 -0700

    Add 3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_node_end - check the code
+ * @head: head
+ * @str: str
+ *
+ * Return: Always 0.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *n_t = malloc(sizeof(list_t));
+
+	if (!head || !n_t)
+		return (NULL);
+	if (str)
+	{
+		n_t->str = strdup(str);
+		if (!n_t)
+		{
+			free(n_t);
+			return (NULL);
+		}
+		n_t->len = strlen(str);
+		n_t->next = NULL;
+	}
+	while (head)
+	{
+		head = head->next;
+	}
+	head->next = n_t;
+	return (n_t);
+}
