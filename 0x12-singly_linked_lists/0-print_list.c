6968c19cc2850037848db5c75fae83d077c5eca2
BahyAllaAllam <bahy.alam@yahoo.com>
2023-09-21 13:05:21 -0700

    Add 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_list - check the code
+ * @h: h
+ *
+ * Return: Always 0.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", strlen((*h).str), h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This the readme file for the task 0x12-singly_linked_lists
