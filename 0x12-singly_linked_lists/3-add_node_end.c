8af9dce823c20388ee1c58ae4de5980744f03b0d
BahyAllaAllam <bahy.alam@yahoo.com>
2023-09-21 16:37:16 -0700

    Add 3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -24,10 +24,10 @@ list_t *add_node_end(list_t **head, const char *str)
 		n_t->len = strlen(str);
 		n_t->next = NULL;
 	}
-	while (head)
+	while (*head)
 	{
-		head = head->next;
+		*head = *head->next;
 	}
-	head->next = n_t;
+	*head->next = n_t;
 	return (n_t);
 }
