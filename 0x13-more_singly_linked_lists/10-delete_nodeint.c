1f68806baaddd04deed93603fc0fddd849a52b1b
BahyAllaAllam <bahy.alam@yahoo.com>
2023-09-26 14:51:51 -0700

    Hot fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	if (idx == 0)
 	{
-		*head = *head->next;
+		*head = (*head)->next;
 		free(temp);
 		return (1);
 	}
