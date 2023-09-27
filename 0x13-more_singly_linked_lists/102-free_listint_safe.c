df4def95bb42463fb4cbf2db194629f65e51a650
BahyAllaAllam <bahy.alam@yahoo.com>
2023-09-26 17:17:30 -0700

    Add 102-free_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,62 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - check the code
+ * @head: h
+ *
+ * Return: Always 0.
+*/
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *ptr, *end;
+
+	if (head == NULL)
+		return (NULL);
+	for (end = head->next; end != NULL; end = end->next)
+	{
+		if (end == end->next)
+			return (end);
+		for (ptr = head; ptr != end; ptr = ptr->next)
+		{
+			if (ptr == end->next)
+				return (end->next)
+		}
+	}
+	return (NULL);
+}
+/**
+ * free_listint_safe - check the code
+ * @h: h
+ *
+ * Return: Always 0.
+*/
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *t, *loop_node;
+	size_t len;
+	int loop = 1;
+
+	if (!h || !*h)
+		return (0);
+	loop_node = find_listint_loop(*h);
+	for (len = 0; (*h != loop_node || loop) && *h != NULL; *h = t)
+	{
+		len++;
+		t = (*h)->next;
+		if (*h == loop_node && loop)
+		{
+			if (loop_node == loop_node->next)
+			{
+				free(*h);
+				break;
+			}
+			len++;
+			t = next->next;
+			free((*h)->next);
+			loop = 0;
+		}
+		free(*h);
+	}
+	*h = NULL;
+	return (len);
+}
