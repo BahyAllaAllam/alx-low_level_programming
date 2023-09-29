761e13ab21835c39e755998b3e4ba6e88588f313
BahyAllaAllam <bahy.alam@yahoo.com>
2023-09-28 18:23:18 -0700

    Add 4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * clear_bit - check the code
+ * @n: n
+ * @index: index
+ *
+ * Return: Always 0.
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(*n) * 8 || n == NULL)
+		return (-1);
+	unsigned long int i = ~(1UL << index);
+	*n &= i;
+	return (1);
+}
