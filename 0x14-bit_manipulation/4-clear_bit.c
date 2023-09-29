c41f63fa4e88a0ebd222aa8d8f63004b569a013c
BahyAllaAllam <bahy.alam@yahoo.com>
2023-09-28 18:37:04 -0700

    Hot fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,11 +9,9 @@
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int i;
-
 	if (index >= sizeof(*n) * 8 || n == NULL)
 		return (-1);
-	i = ~(1UL << index);
-	*n = *n & i;
+	if (*n & 1L << index)
+		*n ^= 1L << index;
 	return (1);
 }
