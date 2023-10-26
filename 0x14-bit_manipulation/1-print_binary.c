363ca52972d2b3bc09ed5b2af55765f49e43cd22
Bahy Allam <66816838+BahyAllaAllam@users.noreply.github.com>
2023-10-27 01:20:17 +0200

    m

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_binary - check the code
+ * @n: n
+ *
+ * Return: Always 0.
+*/
+void print_binary(unsigned long int n)
+{
+	int bit, i = sizeof(unsigned long int) * 8 - 1, flag = 0;
+
+	if (n == 0)
+		putchar('0');
+	for (; i >= 0; i--)
+	{
+		bit = (n >> i) & 1;
+		if (bit == 1)
+		{
+			flag = 1;
+			putchar('1');
+		}
+		else if (flag == 1)
+			putchar('0');
+	}
+}
