2d120cf6b59ed9732d44757d34e5f97cccacb269
BahyAllaAllam <bahy.alam@yahoo.com>
2023-12-07 11:34:06 -0800

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,13 +2,15 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	dlistint_t *temp = h;
+	const dlistint_t *temp = h;
 	size_t i = 0;
+
 	while(temp)
 	{
-		printf("%d\n", temp->data);
+		printf("%d\n", temp->n);
 		temp = temp->next;
 		i++;
 	}
+
 	return (i);
 }
