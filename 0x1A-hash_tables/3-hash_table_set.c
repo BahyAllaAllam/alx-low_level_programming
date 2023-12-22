70a6d12f5bdeffe036df09ec358961a9993c8b10
BahyAllaAllam <bahy.alam@yahoo.com>
2023-12-22 11:40:32 -0800

    3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - check the code
+ * @ht: hash table
+ * @key: key
+ * @value: value of the key
+ * Return: Always EXIT_SUCCESS.
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node = NULL;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
+	new_node->value = (value != NULL) ? strdup(value) : NULL;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
